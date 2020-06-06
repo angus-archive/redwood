f754f5160163c7f20a06620ebc88028f1b0c8053
Angus <Agoody1@sheffield.ac.uk>
2020-06-06 13:13:03 +0100

    Fixed header bug

--- includes/header-content.php
+++ includes/header-content.php
@@ -1,6 +1,6 @@
 <div id="header">
     <div id="logoContainer">
 
-        <a href="index.php"><img id="redLogo" src="<?php echo $_SERVER['DOCUMENT_ROOT'];?>/resources/coreImages/LogoSVG.svg" alt="Redwood Logo"></a>
+        <a href="index.php"><img id="redLogo" src="/resources/coreImages/LogoSVG.svg" alt="Redwood Logo"></a>
     </div>
 </div>
