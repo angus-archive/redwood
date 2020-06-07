f9ab713cc83341e4857760b39586bb67a447662c
Angus <Agoody1@sheffield.ac.uk>
2020-06-07 17:29:02 +0100

    Starting to add HTML for economy page

--- packages/economy.php
+++ packages/economy.php
@@ -16,7 +16,20 @@
 		<div class="spacer"></div>
         <div class="alignDiv">
             <h1 class="pageHeading sTextSpacerB"> ECONOMY PACKAGE</h1>
-        </div>
+			<p>The Economy Package is our cheapest package, suitable for businesses who feel as though they would like to begin to build a social media page, in order to support their operations and inform people of what they do. This package is ideal for smaller business who are looking to build a social media presence whilst on a budget. At Redwood we know that as a small business you need to watch where you spend, so this package offers flexibility on pricing, to help you stay within your budget. </p>
+        	<div>
+				<h3>Advice for better social media posts</h3>
+				<p> This is a brief sentence explaining what the heading point describes, it should be around two or three sentences in length.</p>
+			</div>
+			<div>
+				<h3>Market Analysis</h3>
+				<p> This is a brief sentence explaining what the heading point describes, it should be around two or three sentences in length.</p>
+			</div>
+			<div>
+				<h3>Creative content ideas</h3>
+				<p> This is a brief sentence explaining what the heading point describes, it should be around two or three sentences in length.</p>
+			</div>
+		</div>
     </main>
 
 </div>
