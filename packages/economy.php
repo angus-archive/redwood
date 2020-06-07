e382acf6c99df6ea7750172634087b61038f1e5b
Angus <Agoody1@sheffield.ac.uk>
2020-06-07 19:25:48 +0100

    Splitting packages up

--- packages/economy.php
+++ packages/economy.php
@@ -17,7 +17,13 @@
         <div class="alignDiv">
             <h1 class="pageHeading sTextSpacerB"> ECONOMY PACKAGE</h1>
 			<p>The Economy Package is our cheapest package, suitable for businesses who feel as though they would like to begin to build a social media page, in order to support their operations and inform people of what they do. This package is ideal for smaller business who are looking to build a social media presence whilst on a budget. At Redwood we know that as a small business you need to watch where you spend, so this package offers flexibility on pricing, to help you stay within your budget. </p>
-        	<div>
+        	<!-- Q&A -->
+			<div>
+				<h3>Who is this package for?</h3>
+				<p> Explain who this package is for</p>
+			</div>
+
+			<div>
 				<h3>Advice for better social media posts</h3>
 				<p> This is a brief sentence explaining what the heading point describes, it should be around two or three sentences in length.</p>
 			</div>
