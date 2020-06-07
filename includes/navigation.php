83cbc0732759c9c6e9225a38611d6c2627243f2c
Angus <Agoody1@sheffield.ac.uk>
2020-06-07 21:32:31 +0100

    Update nav bar to a Bootstrap nav

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,17 +1,29 @@
-<div id="navContainer">
-	<div class="burger" id="burgerContainer" onclick="burgerClicked()">
-		<div class="burgerLine1"></div>
-		<div class="burgerLine2"></div>
-		<div class="burgerLine3"></div>
+<nav class="navbar navbar-expand-md navbar-light">
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse justify-content-center" id="collapsibleNavbar">
+		<ul class="navbar-nav nav-justified">
+			<li class="nav-item">
+				<!--HOME PAGE-->
+				<a class="nav-link text-nowrap" href="/index.php" <?php if($CURRENT_PAGE == "Index"){echo "";}?> >Our Services</a>
+			</li>
+			<li class="nav-item">
+				<!--PACKAGES-->
+				<a class="nav-link text-nowrap" href="/packages" <?php if($CURRENT_PAGE == "Packages"){echo "";}?> >Packages</a>
+			</li>
+			<li class="nav-item">
+				<!--OUR STORY-->
+				<a class="nav-link text-nowrap" href="/our-story.php" <?php if($CURRENT_PAGE == "Our-Story"){echo "id=currentNavItem";}?> >Our Story</a>
+			</li>
+			<li class="nav-item">
+				<!--CONTACT-->
+				<a class="nav-link text-nowrap" href="/contact.php" <?php if($CURRENT_PAGE == "Contact"){echo "id=currentNavItem";}?> >Contact</a>
+			</li>
+		</ul>
 	</div>
-	<div id="jsNav" class="navLinkList">
-		<!--HOME PAGE-->
-		<a href="/index.php" <?php if($CURRENT_PAGE == "Index"){echo "id=currentNavItem";}?> >Our Services</a>
-		<!--PACKAGES-->
-		<a href="/packages" <?php if($CURRENT_PAGE == "Packages"){echo "id=currentNavItem";}?> >Packages</a>
-		<!--OUR STORY-->
-		<a href="/our-story.php" <?php if($CURRENT_PAGE == "Our-Story"){echo "id=currentNavItem";}?> >Our Story</a>
-		<!--CONTACT-->
-		<a href="/contact.php" <?php if($CURRENT_PAGE == "Contact"){echo "id=currentNavItem";}?> >Contact</a>
-	</div>
-</div>
+</nav>
+
+
+
+
