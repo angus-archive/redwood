ef76c51d77ab2ecfd805117718418507386b9f9e
Angus <Agoody1@sheffield.ac.uk>
2020-06-09 23:46:48 +0100

    Added footer to optional extras to include in package

--- includes/optional-extras.php
+++ includes/optional-extras.php
@@ -3,13 +3,16 @@
 <div class="container pt-sm-4">
     <div class="row">
         <div class="col-lg-3 mb-5">
-            <div class="card text-center">
+            <div class="card text-center packageShadow">
                 <div class="card-body">
                     <img class="mb-2" src="https://via.placeholder.com/90">
-                    <h3 class="mt-sm-2">Meetings</h3>
+                    <h3 class="mt-sm-2">Photography Sessions</h3>
                     <div class="featureDivider"></div>
-                    <p class="p-1"> This is a brief description of the feature</p>
+                    <p class="p-1">Showcase your products or services with professional pictures</p>
                 </div>
+				<div class="card-footer redwoodBackground text-white p-1">
+					<p class="m-auto small">Free with Redwood Package</p>
+				</div>
             </div>
         </div>
         <div class="col-lg-3 mb-5">
