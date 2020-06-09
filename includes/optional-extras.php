4f3ffafed5bc93dd7fcdad5e28b4616c63e25af5
Angus <Agoody1@sheffield.ac.uk>
2020-06-10 00:12:59 +0100

    Filled out names for optional extras

--- includes/optional-extras.php
+++ includes/optional-extras.php
@@ -2,88 +2,78 @@
 <!-- Container -->
 <div class="container pt-sm-4">
     <div class="row">
-        <div class="col-lg-3 mb-5">
-            <div class="card text-center packageShadow">
-                <div class="card-body">
-                    <img class="mb-2" src="https://via.placeholder.com/90">
-                    <h3 class="mt-sm-2">Photography Sessions</h3>
-                    <div class="featureDivider"></div>
-                    <p class="p-1">Showcase your products or services with professional pictures</p>
-                </div>
+		<div class="col-lg-3 mb-5">
+			<div class="card text-center packageShadow">
+				<div class="card-body">
+					<img class="mb-2" src="https://via.placeholder.com/90">
+					<h3 class="mt-sm-2">Microsoft Masterclass</h3>
+					<div class="featureDivider"></div>
+					<p class="p-1">Learn how to use Microsoft Office</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-lg-3 mb-5">
+			<div class="card text-center packageShadow">
+				<div class="card-body">
+					<img class="mb-2" src="https://via.placeholder.com/90">
+					<h3 class="mt-sm-2">Logo Design</h3>
+					<div class="featureDivider"></div>
+					<p class="p-1">A fresh new logo for your business</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-lg-3 mb-5">
+			<div class="card text-center packageShadow">
+				<div class="card-body">
+					<img class="mb-2" src="https://via.placeholder.com/90">
+					<h3 class="mt-sm-2">Photography Sessions</h3>
+					<div class="featureDivider"></div>
+					<p class="p-1">Showcase your products or services with professional pictures</p>
+				</div>
 				<div class="card-footer redwoodBackground text-white p-1">
 					<p class="m-auto small">Free with Redwood Package</p>
 				</div>
-            </div>
-        </div>
-        <div class="col-lg-3 mb-5">
-            <div class="card text-center">
-                <div class="card-body">
-                    <img class="mb-2" src="https://via.placeholder.com/90">
-                    <h3 class="mt-sm-2">Advice</h3>
-                    <div class="featureDivider"></div>
-                    <p class="p-1"> This is a brief description of the feature</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-3 mb-5">
-            <div class="card text-center">
-                <div class="card-body">
-                    <img class="mb-2" src="https://via.placeholder.com/90">
-                    <h3 class="mt-sm-2">Ideas</h3>
-                    <div class="featureDivider"></div>
-                    <p class="p-1"> This is a brief description of the feature</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-3 mb-5">
-            <div class="card text-center">
-                <div class="card-body">
-                    <img class="mb-2" src="https://via.placeholder.com/90">
-                    <h3 class="mt-sm-2">Calendar</h3>
-                    <div class="featureDivider"></div>
-                    <p class="p-1"> This is a brief description of the feature</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-3 mb-5">
-            <div class="card text-center">
-                <div class="card-body">
-                    <img class="mb-2" src="https://via.placeholder.com/90">
-                    <h3 class="mt-sm-2">Calendar</h3>
-                    <div class="featureDivider"></div>
-                    <p class="p-1"> This is a brief description of the feature</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-3 mb-5">
-            <div class="card text-center">
-                <div class="card-body">
-                    <img class="mb-2" src="https://via.placeholder.com/90">
-                    <h3 class="mt-sm-2">Calendar</h3>
-                    <div class="featureDivider"></div>
-                    <p class="p-1"> This is a brief description of the feature</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-3 mb-5">
-            <div class="card text-center">
-                <div class="card-body">
-                    <img class="mb-2" src="https://via.placeholder.com/90">
-                    <h3 class="mt-sm-2">Calendar</h3>
-                    <div class="featureDivider"></div>
-                    <p class="p-1"> This is a brief description of the feature</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-3 mb-5">
-            <div class="card text-center">
-                <div class="card-body">
-                    <img class="mb-2" src="https://via.placeholder.com/90">
-                    <h3 class="mt-sm-2">Calendar</h3>
-                    <div class="featureDivider"></div>
-                    <p class="p-1"> This is a brief description of the feature</p>
-                </div>
-            </div>
-        </div>
+			</div>
+		</div>
+		<div class="col-lg-3 mb-5">
+			<div class="card text-center packageShadow">
+				<div class="card-body">
+					<img class="mb-2" src="https://via.placeholder.com/90">
+					<h3 class="mt-sm-2">Financial Advice</h3>
+					<div class="featureDivider"></div>
+					<p class="p-1">Get financial tips and advice for running your business</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-lg-4 mb-5">
+			<div class="card text-center packageShadow">
+				<div class="card-body">
+					<img class="mb-2" src="https://via.placeholder.com/90">
+					<h3 class="mt-sm-2">Branding</h3>
+					<div class="featureDivider"></div>
+					<p class="p-1">Stand out from your competition with our branding service</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-lg-4 mb-5">
+			<div class="card text-center packageShadow">
+				<div class="card-body">
+					<img class="mb-2" src="https://via.placeholder.com/90">
+					<h3 class="mt-sm-2">Opportunity Reach Analysis</h3>
+					<div class="featureDivider"></div>
+					<p class="p-1">Whatever that is, provide a description here</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-lg-4 mb-5">
+			<div class="card text-center packageShadow">
+				<div class="card-body">
+					<img class="mb-2" src="https://via.placeholder.com/90">
+					<h3 class="mt-sm-2">Leads Investigation</h3>
+					<div class="featureDivider"></div>
+					<p class="p-1">Again, whatever that is, put a description here</p>
+				</div>
+			</div>
+		</div>
     </div>
 </div>
