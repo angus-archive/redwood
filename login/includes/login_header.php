11b9c1dc6e2a94bb35c79cd487375d4dcd26c6c6
Angus <Agoody1@sheffield.ac.uk>
2020-06-18 17:46:09 +0100

    Added redirect from index if logged in and working logout script

--- login/includes/login_header.php
+++ login/includes/login_header.php
@@ -3,8 +3,14 @@
 		<div id="logoContainer">
 			<a href="/login/home.php"><img id="redLogo" src="/resources/coreImages/LogoSVG.svg" alt="Redwood Logo"></a>
 		</div>
-		<div class="p-2 d-flex justify-content-center align-items-center" style="background-color:#3885BB">
+		<div class="p-2 d-flex justify-content-center align-items-center position-relative" style="background-color:#3885BB">
 			<h1 class="text-center text-white mb-0" style="font-size: 1.5rem">Employee Section</h1>
+			<?php
+			// If the user is not logged in redirect to the login page...
+			if (isset($_SESSION['loggedin'])) {
+				echo "<a href=\"/login/logout.php\" class=\"btn btn-light position-absolute\" style=\"right: 10px\">Logout</a>";
+			}
+			?>
 		</div>
 	</div>
 </header>
