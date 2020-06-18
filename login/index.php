63a560a16d931a1678506cb18933939ca601e790
Angus <Agoody1@sheffield.ac.uk>
2020-06-18 22:06:59 +0100

    Fixed redirect bug

--- login/index.php
+++ login/index.php
@@ -1,4 +1,12 @@
-<?php $path = $_SERVER['DOCUMENT_ROOT'];$path .= "/login/includes/auth_redirect.php";include_once($path); ?>
+<?php
+// We need to use sessions, so you should always start sessions using the below code.
+session_start();
+// If the user is not logged in redirect to the login page...
+if (isset($_SESSION['loggedin'])) {
+	header('Location: /login/home.php');
+	exit;
+}
+?>
 <?php $path = $_SERVER['DOCUMENT_ROOT'];$path .= "/includes/a_config.php";include_once($path); ?>
 <!DOCTYPE html>
 <html lang="en">
