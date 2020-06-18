860d308c668d929f14e2e506ed22adae008c69dc
Angus <Agoody1@sheffield.ac.uk>
2020-06-18 23:52:19 +0100

    Slight UI changed and changed ID

--- login/includes/login_navbar.php
+++ login/includes/login_navbar.php
@@ -1,9 +1,10 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+	<a class="navbar-brand" href="/login/home.php"><img src="/resources/coreImages/LogoEmploy.svg" alt="Redwood logo" style="width: 200px; height: 40px"></a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#redwoodLoginNav" aria-controls="redwoodLoginNav" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 	</button>
-	<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-		<a class="navbar-brand" href="/login/home.php"><img src="/resources/coreImages/LogoEmploy.svg" alt="Redwood logo" style="width: 200px; height: 40px"></a>
+
+	<div class="collapse navbar-collapse" id="redwoodLoginNav">
 		<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
 			<li class="nav-item active">
 				<a class="nav-link" href="/login/home.php">Home <span class="sr-only">(current)</span></a>
@@ -16,8 +17,7 @@
 			</li>
 		</ul>
 		<form class="form-inline my-2 my-lg-0">
-			<a href="/login/logout.php" class="btn btn-danger">Logout</a>
+			<a href="/login/logout.php" class="btn redwoodBackground redwoodHover text-light">Logout</a>
 		</form>
-
 	</div>
 </nav>
