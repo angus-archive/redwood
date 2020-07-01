28fa7df8c2dbfd14351a3fe8d95879b46bb127b2
Angus <angus.goody@gmail.com>
2020-07-01 23:48:08 +0100

    Switched from www to non www

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<head>
 		<!-- Head tags -->
 		<?php $path = $_SERVER['DOCUMENT_ROOT'];$path .= "/includes/head-tags.php";include_once($path); ?>
-		<link rel="canonical" href="https://www.redwood.business/"/>
+		<link rel="canonical" href="https://redwood.business/"/>
 	</head>
 
 	<body>
