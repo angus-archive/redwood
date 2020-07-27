73a8b56f96719dff2b3e4eea8ed4d0e155453240
Angus <angus.goody@gmail.com>
2020-07-27 19:45:56 +0100

    Fixed DB bug

--- login/functions/connect-to-database.php
+++ login/functions/connect-to-database.php
@@ -9,7 +9,7 @@ if (file_exists($secret_file)) {
     $DATABASE_HOST = get_redwood_db_host();
     $DATABASE_USER = get_redwood_db_username();
     $DATABASE_PASS = get_redwood_db_password();
-    $DATABASE_NAME = 'redwood';
+    $DATABASE_NAME = get_redwood_db_name();
     try {
       $pdo = new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
     } catch (PDOException $exception) {
