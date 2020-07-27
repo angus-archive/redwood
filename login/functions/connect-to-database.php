3913b37556c7cb26fa2fd1510feda04aaf7bab41
Angus <angus.goody@gmail.com>
2020-07-27 18:55:14 +0100

    Simplified Connect to Database file

--- login/functions/connect-to-database.php
+++ login/functions/connect-to-database.php
@@ -1,7 +1,24 @@
 <?php
 //Start a PHP session
 session_start();
-// Include functions and connect to the database using PDO MySQL
-include_once 'pdo-connect.php';
-$pdo = pdo_connect_mysql();
+
+//Import secret keys
+$secret_file=dirname($_SERVER["DOCUMENT_ROOT"])."/private_data/secret_keys.php";
+if (file_exists($secret_file)) {
+    require $secret_file;
+    $DATABASE_HOST = get_redwood_db_host();
+    $DATABASE_USER = get_redwood_db_username();
+    $DATABASE_PASS = get_redwood_db_password();
+    $DATABASE_NAME = 'redwood';
+    try {
+      $pdo = new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
+    } catch (PDOException $exception) {
+      // If there is an error with the connection, stop the script and display the error.
+      header('location: /login?connection=failed');
+    }
+
+}
+else {
+    header('location: /login?connection=failed');
+}
 ?>
