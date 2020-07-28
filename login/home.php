d8ec1325aed5d62a2c130ad8db598f609e1e4eb7
Angus <angus.goody@gmail.com>
2020-07-28 16:47:57 +0100

    Adding task view for users

--- login/home.php
+++ login/home.php
@@ -61,60 +61,108 @@ if(isset($_GET["sent"])){
 				</div>
 			</div>
 		</div>
-
 		<?php if($_SESSION["user_type"] == "boss"):?>
-				<!--Boss man section -->
-				<div class="container">
-					<div class="text-center">
-						<h4> Assign tasks </h4>
-						<p> As Bossman you can assign tasks to other team members here </p>
+		<!--Boss man section -->
+		<div class="container">
+			<div class="text-center">
+				<h4> Assign tasks </h4>
+				<p> As Bossman you can assign tasks to other team members here </p>
+			</div>
+			<?php if ($sent === "true"):?>
+			<div class="alert alert-success" role="alert">
+			  Task Sent
+			</div>
+			<?php endif; ?>
+			<!-- Task form -->
+			<form action="/login/functions/send-tasks" method="post">
 
-					</div>
-					<?php if ($sent === "true"):?>
-					<div class="alert alert-success" role="alert">
-					  Task Sent
-					</div>
-					<?php endif; ?>
-					<!-- Task form -->
-					<form action="/login/functions/send-tasks" method="post">
-
-						<!-- Task Name -->
-						<div class="form-group">
-						    <label for="taskName">Task Name</label>
-						    <input name="task_name" type="text" class="form-control" id="taskName" required>
-						</div>
-						<!-- Task Urgency -->
-					  <div class="form-group">
-					    <label for="urgency">Urgency</label>
-					    <select name="task_urgency" class="form-control" id="urgency">
-					      <option>Urgent</option>
-					      <option>Quite Important</option>
-					      <option>No Rush</option>
-					    </select>
-					  </div>
-					 	
-					 	<!-- Task Description -->
-					  <div class="form-group">
-					    <label for="task_description">Task Description</label>
-					    <textarea name="task_description" class="form-control" id="task_description" rows="3" required></textarea>
-					  </div>
-
-				  	<!-- Employee Select -->
-				    <div class="form-group">
-				      <label for="employee_select">Select Employee</label>
-				      <select name="task_employee" class="form-control" id="employee_select">
-				      	<?php foreach ($employee_names as $e): ?>
-				      		<option><?=$e["username"]?></option>
-				      	<? endforeach; ?>
-				      </select>
-				    </div>
-					  
-					  <!--Submit -->
-					  <button type="submit" class="btn btn-primary">Send</button>
-					</form>
+				<!-- Task Name -->
+				<div class="form-group">
+				    <label for="taskName">Task Name</label>
+				    <input name="task_name" type="text" class="form-control" id="taskName" required>
 				</div>
-		<?php endif; ?>
+				<!-- Task Urgency -->
+			  <div class="form-group">
+			    <label for="urgency">Urgency</label>
+			    <select name="task_urgency" class="form-control" id="urgency">
+			      <option>Urgent</option>
+			      <option>Quite Important</option>
+			      <option>No Rush</option>
+			    </select>
+			  </div>
+			 	
+			 	<!-- Task Description -->
+			  <div class="form-group">
+			    <label for="task_description">Task Description</label>
+			    <textarea name="task_description" class="form-control" id="task_description" rows="3" required></textarea>
+			  </div>
 
+		  	<!-- Employee Select -->
+		    <div class="form-group">
+		      <label for="employee_select">Select Employee</label>
+		      <select name="task_employee" class="form-control" id="employee_select">
+		      	<?php foreach ($employee_names as $e): ?>
+		      		<option><?=$e["username"]?></option>
+		      	<? endforeach; ?>
+		      </select>
+		    </div>
+			  
+			  <!--Submit -->
+			  <button type="submit" class="btn btn-primary">Send</button>
+			</form>
+		</div>
+		<?php elseif($_SESSION["user_type"] == "user"): ?>
+		<!--User section -->
+		<div class="container">
+			<!--Top Text -->
+			<div class="text-center">
+				<h4> Your Tasks </h4>
+				<p> Any tasks that bossman has set you will appear here </p>
+			</div>
+
+			<!-- Tabs Container -->
+			<div class="border p-4 bg-white">
+				<!-- Tabs Menu -->
+				<nav>
+				  <div class="nav nav-tabs" id="nav-tab" role="tablist">
+				    <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Tasks</a>
+				    <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Completed Tasks</a>
+				  </div>
+				</nav>
+				<!-- Tabs Content -->
+				<div class="tab-content py-4" id="nav-tabContent">
+					<!-- All tasks -->
+				  <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+				  	<div class="row">
+				  		<!--Example -->
+				  		<div class="col-lg-4">
+				  			<div class="card">
+				  			  <div class="card-body">
+				  			    <h5 class="card-title">Task Title</h5>
+				  			    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				  			  </div>
+				  			  <ul class="list-group list-group-flush">
+				  			    <li class="list-group-item d-flex justify-content-between align-items-center"><span>Urgency:</span><span class="badge badge-danger">Urgent</span></li>
+				  			    <li class="list-group-item d-flex justify-content-between align-items-center"><span>Date added:</span><span>25/06/2020</span></li>
+				  			    <li class="list-group-item d-flex justify-content-between align-items-center"><span>Status:</span><span>Incomplete</span></li>
+				  			  </ul>
+				  			  <div class="card-body text-center">
+				  			    <a href="#" class="card-link">Mark as complete</a>
+				  			  </div>
+				  			</div>
+				  		</div>
+				  	</div>
+				  </div>
+				  <!-- Completed tasks -->
+				  <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+				  	<h5> Completed </h5>
+				  </div>
+				</div>
+			</div>
+
+		
+
+		<?php endif; ?>
 		<!-- Spacer-->
 		<div class="pSpacer-y-40"></div>
 
@@ -123,6 +171,7 @@ if(isset($_GET["sent"])){
 	<?php $path = $_SERVER['DOCUMENT_ROOT'];$path .= "/includes/footer.php";include_once($path); ?>
 	<!-- Footer - Tags -->
 	<?php $path = $_SERVER['DOCUMENT_ROOT'];$path .= "/includes/footer-tags.php";include_once($path); ?>
+
 </div>
 </body>
 
--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -6,10 +6,3 @@ Main Javascript File
 */
 
 
-//When the user clicks the "burger" icon in mobile nav mode
-function burgerClicked(){
-    //Show the drop down links
-    document.getElementById("jsNav").classList.toggle("navOpen");
-    //Update CSS to animate burger
-    document.getElementById("burgerContainer").classList.toggle("burgerChange");
-}
